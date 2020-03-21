ea49ffddfcccaf12e8758c4b002f2ed0b28b7dd7
foresightyj <imyuanjian@gmail.com>
2020-03-21 14:59:30 +0800

    minor

--- test_file.js
+++ test_file.js
@@ -4,19 +4,20 @@ const stylelint = require('stylelint');
 
 const path = process.argv[2];
 
-stylelint.lint({
-    files: [path],
-    config: {
-        plugins: [
-            "./src/index.js"
-        ],
-        rules: {
-            "fht-rules/fht-stylelint-comment-rule": [true, {
-                ignores: ['vendor'],
-            }],
+(async () => {
+    const resultObject = await stylelint.lint({
+        files: [path],
+        config: {
+            plugins: [
+                "./src/index.js"
+            ],
+            rules: {
+                "fht-rules/fht-stylelint-comment-rule": [true, {
+                    ignores: ['vendor'],
+                }],
+            }
         }
-    }
-}).then(function (resultObject) {
+    });
     if (resultObject.errored) {
         for (const result of resultObject.results) {
             for (const w of result.warnings) {
@@ -24,4 +25,4 @@ stylelint.lint({
             }
         }
     }
-});
+})();
